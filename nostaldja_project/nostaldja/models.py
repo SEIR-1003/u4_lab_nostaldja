f19b58d2913c573dc288a1ba9c44c52c7e0b3195
Lerenzo  Martinez <lerenzomartinez@Lerenzos-MacBook-Air.local>
2022-12-15 17:39:22 -0600

    completed

---
+++ nostaldja_project/nostaldja/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Decade(models.Model):
+    name = models.CharField(max_length=100)
+    start_year = models.CharField(max_length=100)
+    end_year = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Fads(models.Model):
+    name = models.CharField(max_length=100)
+    image_url = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    decade = models.ForeignKey(Decade, on_delete=models.CASCADE, related_name='fads')
+
+    def __str__(self):
+        return self.name
+
+# class Fads(models.Model):
+#     decade = models.ForeignKey(Decade, on_delete=models.CASCADE, related_name='fads')
---
+++ nostaldja_project/settings.sql
@@ -0,0 +1,4 @@
+
+CREATE DATABASE nostaldja;
+CREATE USER nostaldjauser WITH PASSWORD 'nostaldja';
+GRANT ALL PRIVILEGES ON DATABASE nostaldja TO nostaldjauser;
