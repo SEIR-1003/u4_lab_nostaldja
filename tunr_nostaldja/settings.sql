ada9fa79d6775df156cb640ffba5bd2d36227275
Brandon Chan <bchan26@bu.edu>
2022-12-21 01:44:01 -0500

    classwork

--- tunr_nostaldja/settings.sql
+++ tunr_nostaldja/settings.sql
@@ -2,3 +2,5 @@
 CREATE DATABASE nostaldja;
 CREATE USER tunruser WITH PASSWORD 'tunr';
 GRANT ALL PRIVILEGES ON DATABASE nostaldja TO tunruser;
+--only needed if -U postgres is needed
+GRANT postgres TO tunruser;
